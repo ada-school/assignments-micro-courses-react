504c54180c7ebd28b1c14c8cd34f7b7c2876e13b
ErikCasas <santicasas667@gmail.com>
2024-02-11 19:50:02 -0500

    base code for the students

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
-export const myName = ''
+import {
+  firstPicture,
+  secondPicture,
+  thirdPicutre,
+  fourthPicutre,
+} from './data'
 
 export function App() {
-  return <div>Hello world, my name is </div>
+  return (
+      <section>
+        <article>
+          <h5>{firstPicture.paintingName}</h5>
+          <img src={firstPicture.imgURL} alt='first' />
+          <p>{firstPicture.author}</p>
+        </article>
+
+        <article>
+          <h5>{secondPicture.paintingName}</h5>
+          <img src={secondPicture.imgURL} alt='second' />
+          <p>{secondPicture.author}</p>
+        </article>
+
+        <article>
+          <h5>{thirdPicutre.paintingName}</h5>
+          <img src={thirdPicutre.imgURL} alt='third' />
+          <p>{thirdPicutre.author}</p>
+        </article>
+
+        <article>
+          <h5>{fourthPicutre.paintingName}</h5>
+          <img src={fourthPicutre.imgURL} alt='fourth' />
+          <p>{fourthPicutre.author}</p>
+        </article>
+      </section>
+  )
 }
