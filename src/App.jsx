1c48c4b2ef05fdea7b5ec9b263def6f0dab68a04
ErikCasas <santicasas667@gmail.com>
2024-02-13 11:48:22 -0500

    clean base code

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,7 @@
 export function App() {
   return (
-    <section>
-      <article>
-        <h5>
-          Ada King (1815–1852), Countess of Lovelace, Mathematician, Daughter of
-          Lord Byron
-        </h5>
-        <img
-          alt='first'
-          src='https://upload.wikimedia.org/wikipedia/commons/8/87/Ada_Lovelace.jpg'
-        />
-        <p>Margaret Sarah Carpenter</p>
-      </article>
-      <article>
-        <h5>We Can Do It!</h5>
-        <img
-          alt='first'
-          src='https://upload.wikimedia.org/wikipedia/commons/1/12/We_Can_Do_It%21.jpg'
-        />
-        <p>J. Howard Miller</p>
-      </article>
-      <article>
-        <h5>La joven de la perla</h5>
-        <img
-          alt='first'
-          src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Meisje_met_de_parel.jpg/800px-Meisje_met_de_parel.jpg'
-        />
-        <p>Johannes Vermeer</p>
-      </article>
-      <article>
-        <h5>Autoretrato con mono</h5>
-        <img
-          alt='first'
-          src='https://historia-arte.com/_/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpbSI6WyJcL2FydHdvcmtcL2ltYWdlRmlsZVwvNWYyNTMxM2UyZGUyZC5qcGciLCJyZXNpemUsODAwIl19.-dJRrycn7cE7szQihpGdLGy0woa-4K0SSluurohZlwE.jpg'
-        />
-        <p>Frida Kahlo</p>
-      </article>
-    </section>
+    <>
+     
+    </>
   )
 }
