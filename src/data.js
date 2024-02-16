ea5f9342e7db05e0dda24654a3b26d900767bf8a
ErikCasas <santicasas667@gmail.com>
2024-02-16 00:06:39 -0500

    authorDescription for the pictures

--- src/data.js
+++ src/data.js
@@ -1,25 +1,27 @@
 export const firstPicture = {
-  imgURL:
-    'https://upload.wikimedia.org/wikipedia/commons/8/87/Ada_Lovelace.jpg',
+  imgURL: 'https://upload.wikimedia.org/wikipedia/commons/8/87/Ada_Lovelace.jpg',
   author: 'Margaret Sarah Carpenter',
-  paintingName:
-    'Ada King (1815–1852), Countess of Lovelace, Mathematician, Daughter of Lord Byron',
+  paintingName: 'Ada King (1815–1852), Countess of Lovelace, Mathematician, Daughter of Lord Byron',
+  authorDescription: 'Margaret Sarah Carpenter (1793–1872) fue una pintora británica conocida por sus retratos. Fue una artista destacada de su época, exponiendo en la Royal Academy y otras instituciones importantes. Su trabajo captura la esencia de sus sujetos con un realismo y sensibilidad notables.'
 }
+
 export const secondPicture = {
-  imgURL:
-    'https://upload.wikimedia.org/wikipedia/commons/1/12/We_Can_Do_It%21.jpg',
+  imgURL: 'https://upload.wikimedia.org/wikipedia/commons/1/12/We_Can_Do_It%21.jpg',
   author: 'J. Howard Miller',
-  paintingName: 'We Can Do It! ',
+  paintingName: 'We Can Do It!',
+  authorDescription: 'J. Howard Miller fue un artista gráfico estadounidense. Es mejor conocido por su cartel de la Segunda Guerra Mundial "We Can Do It!", destinado a aumentar la moral de los trabajadores. La pieza se ha convertido en un ícono de la cultura popular y un símbolo del empoderamiento femenino.'
 }
+
 export const thirdPicture = {
-  imgURL:
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Meisje_met_de_parel.jpg/800px-Meisje_met_de_parel.jpg',
+  imgURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Meisje_met_de_parel.jpg/800px-Meisje_met_de_parel.jpg',
   author: 'Johannes Vermeer',
   paintingName: 'La joven de la perla',
+  authorDescription: 'Johannes Vermeer (1632–1675) fue un pintor holandés del Barroco. Es reconocido por sus obras detalladas que capturan la luz y la vida cotidiana con una técnica exquisita. "La joven de la perla" es una de sus obras más famosas, apreciada por su uso magistral de la luz y la sombra.'
 }
+
 export const fourthPicture = {
-  imgURL:
-    'https://historia-arte.com/_/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpbSI6WyJcL2FydHdvcmtcL2ltYWdlRmlsZVwvNWYyNTMxM2UyZGUyZC5qcGciLCJyZXNpemUsODAwIl19.-dJRrycn7cE7szQihpGdLGy0woa-4K0SSluurohZlwE.jpg',
+  imgURL: 'https://historia-arte.com/_/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpbSI6WyJcL2FydHdvcmtcL2ltYWdlRmlsZVwvNWYyNTMxM2UyZGUyZC5qcGciLCJyZXNpemUsODAwIl19.-dJRrycn7cE7szQihpGdLGy0woa-4K0SSluurohZlwE.jpg',
   author: 'Frida Kahlo',
   paintingName: 'Autoretrato con mono',
+  authorDescription: 'Frida Kahlo (1907–1954) fue una pintora mexicana conocida por sus autorretratos llenos de color y simbolismo. A través de su arte, exploró temas como la identidad, la poscolonialidad, el género, la clase y la raza en la sociedad mexicana. Kahlo es celebrada como una figura emblemática del arte nacional e internacional.'
 }
