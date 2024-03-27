40b2bbc7634e6feba3a1fa2f94c40935b7287377
ErikCasas <santicasas667@gmail.com>
2024-03-27 09:58:16 -0500

    base code for evaluation project

---
+++ src/data/charactersInformation.js
@@ -0,0 +1,27 @@
+export const charactersInformation = [
+  {
+    name: 'Seita Yokokawa',
+    imageURL:
+      'https://static.wikia.nocookie.net/studio-ghibli/images/2/21/Seita_Yokokawa.jpg/revision/latest?cb=20210215103515',
+  },
+  {
+    name: 'Setsuko Yokokawa',
+    imageURL:
+      'https://static.wikia.nocookie.net/studio-ghibli/images/a/a8/Setsuko_Yokokawa.jpg/revision/latest?cb=20210215103612',
+  },
+  {
+    name: 'Mrs. Yokokawa',
+    imageURL:
+      'https://static.wikia.nocookie.net/studio-ghibli/images/4/4a/Seita_Mother.png/revision/latest?cb=20110701061823',
+  },
+  {
+    name: 'Kiyoshi Yokokawa',
+    imageURL:
+      'https://static.wikia.nocookie.net/studioghibli/images/e/e4/Kiyoshi_Yokokawa.jpg/revision/latest?cb=20200629051742&path-prefix=es',
+  },
+  {
+    name: "Seita's aunt",
+    imageURL:
+      'https://static.wikia.nocookie.net/studio-ghibli/images/1/1b/Seita_Aunt.jpg/revision/latest?cb=20110701062623',
+  },
+]
---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+//titulos presentes
+export const currentTitles = 22
+
+//productora
+export const producer = {
+  title: 'Producer',
+  information: 'Toru Hara',
+}
+
+//fecha de lanzamiento
+export const realeseDate = {
+  title: 'Realese date',
+  information: 1988,
+}
+
+//Director
+export const director = {
+  title: 'Director',
+  information: 'Isao Takahata',
+}
+
+//icono estudio Ghibli
+export const icon = {
+  imageURL:
+    'https://static.wikia.nocookie.net/studioghibli/images/d/d4/Studio_Ghibli.png/revision/latest/scale-to-width-down/439?cb=20200410033324&path-prefix=es',
+  name: 'Icon Studio Ghibli',
+}
