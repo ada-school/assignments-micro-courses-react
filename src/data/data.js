f1e7f575b5e45dd0cc5933ee74dd7ceda954edf1
ErikCasas <santicasas667@gmail.com>
2024-07-12 08:36:57 -0500

    better data

--- src/data/data.js
+++ src/data/data.js
@@ -1,34 +1,36 @@
 //titulos presentes
-export const currentTitles = 22
+export const currentTitles = 22;
+
+export const movieName = "Grave of the Fireflies";
 
 //productora
 export const producer = {
-	title: 'Producer',
-	information: 'Toru Hara',
-}
+  title: "Producer",
+  information: "Toru Hara",
+};
 
 //fecha de lanzamiento
 export const realeseDate = {
-	title: 'Realese date',
-	information: 1988,
-}
+  title: "Realese date",
+  information: 1988,
+};
 
-//Director
+//director
 export const director = {
-	title: 'Director',
-	information: 'Isao Takahata',
-}
+  title: "Director",
+  information: "Isao Takahata",
+};
 
 //icono estudio Ghibli
 export const icon = {
-	imageURL:
-		'https://seeklogo.com/images/S/Studio_Ghibli-logo-78E2716B50-seeklogo.com.png',
-	name: 'Icon Studio Ghibli',
-}
+  imageURL:
+    "https://seeklogo.com/images/S/Studio_Ghibli-logo-78E2716B50-seeklogo.com.png",
+  name: "Icon Studio Ghibli",
+};
 
-//
+//poster de la pelicula y nombre
 export const posterMovie = {
-	imageURL:
-		'https://media.ada-school.org/5fcd3ac12b22eab4d301d819/64341b68339e6a75c738ef01/dall_e_2024_04_18_08_35_58___a_vertical_movie_poster_depicting_a_21_year_old_boy_and_his_7_year_old_sister_during_wartime__styled_in_the_manner_of_classic_japanese_animation__the_-a58872ca-1535-4524-bedd-4db1feebb866.webp',
-	name: 'Grave of the fireflies',
-}
+  imageURL:
+    "https://media.ada-school.org/5fcd3ac12b22eab4d301d819/64341b68339e6a75c738ef01/dall_e_2024_04_18_08_35_58___a_vertical_movie_poster_depicting_a_21_year_old_boy_and_his_7_year_old_sister_during_wartime__styled_in_the_manner_of_classic_japanese_animation__the_-a58872ca-1535-4524-bedd-4db1feebb866.webp",
+  name: "Grave of the fireflies",
+};
